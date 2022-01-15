078d9bc5f47e5513a3e6933a0dc17d19336d8e82
sadie0925 <sally530135@gmail.com>
2022-01-15 10:46:17 -0500

    Integrate my file into Github

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Credit Card Validator Project
- 
+ Creating credit card validator to check card validation, expiration date and card issuesrs
---
+++ script.js
@@ -0,0 +1,51 @@
+function  LuhnAlgorithm ( unsigned cardNumber );
+function main ();
+
+function LuhnAlgorithm ( unsigned cardNumber ) {
+    var unsigned oddsum;
+    var unsigned evensum;
+    var unsigned finalSum;
+    var unsgined countDigit;
+    countDigit=floor(log10(cardNum)+1);
+    while(countDigit<15||countDigit>19||countDigit==17){
+        console.log ("This is not valid credit card number. Please enter your Valid credit card number: ");
+        cin >> cardNum;
+        countDigit=floor(log10(cardNum)+1);
+    }
+    unsigned long long int storeNum = cardNum;
+    while (cardNum > 0) {
+        int oddNum = cardNum % 10;
+        oddsum += oddNum;
+        cardNum /= 100;
+    }
+
+    storeNum /= 10;
+    while (storeNum > 0) {
+        int evenNum = storeNum % 10;
+        int digit = evenNum * 2;
+        if (digit >= 10) {
+            int firstD = digit / 10;
+            int secondD = digit % 10;
+            digit = firstD + secondD;
+        }
+        evensum += digit;
+        storeNum /= 100;
+    }
+    finalSum = oddsum + evensum;
+    return finalSum;
+}
+
+int main() {
+    unsigned long long int n{0};
+    int result{0};
+    cout << "Please enter your credit card Number: ";
+    cin >> n;
+    result = LuhnAlgorithm ( n );
+    if ( result%10 != 0 ) {
+        cout << "Invalid credit card number! " << endl;
+    }
+    else {
+        cout << "Valid credit card number!" << endl;
+    }
+    return 0;
+}
